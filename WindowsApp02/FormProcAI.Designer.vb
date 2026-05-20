de03ff963b7f394d3c55633974273c1094e2bbbe
Nijiho Higuchi <hdc252317@gmail.com>
2026-05-20 12:30:58 +0900

    ProcA処理１画面を作成

--- WindowsApp02/FormProcAI.Designer.vb
+++ WindowsApp02/FormProcAI.Designer.vb
@@ -22,9 +22,30 @@ Partial Class FormProcAI
     'コード エディターを使って変更しないでください。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(341, 122)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(134, 18)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Proc１の処理だよ"
+        '
+        'FormProcAI
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 18.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "FormProcAI"
         Me.Text = "FormProcAI"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents Label1 As Label
 End Class

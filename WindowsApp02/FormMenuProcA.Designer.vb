7362650a03fcddc8c9d4f3aa71b178f54b7f4b97
Nijiho Higuchi <hdc252317@gmail.com>
2026-05-20 12:25:41 +0900

    Proc2メニュー画面を作成

--- WindowsApp02/FormMenuProcA.Designer.vb
+++ WindowsApp02/FormMenuProcA.Designer.vb
@@ -22,9 +22,41 @@ Partial Class FormMenuProcA
     'コード エディターを使って変更しないでください。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(262, 119)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(200, 51)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "処理１"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(261, 238)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(201, 59)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "処理２"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'FormMenuProcA
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 18.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "FormMenuProcA"
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "FormMenuProcA"
+        Me.Text = "ProcAメニュー"
+        Me.ResumeLayout(False)
+
     End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
 End Class

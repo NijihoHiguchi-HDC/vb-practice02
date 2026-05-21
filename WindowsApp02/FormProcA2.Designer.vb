a3e53c226a6e5feec89f8bfe8ce48f78e09f169d
Nijiho Higuchi <hdc252317@gmail.com>
2026-05-21 15:32:49 +0900

    ProcA2の画面を作成

--- WindowsApp02/FormProcA2.Designer.vb
+++ WindowsApp02/FormProcA2.Designer.vb
@@ -22,9 +22,30 @@ Partial Class FormProcA2
     'コード エディターを使って変更しないでください。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(239, 175)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(177, 18)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "ProcA２つ目の処理だよ"
+        '
+        'FormProcA2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 18.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "FormProcA2"
         Me.Text = "FormProcA2"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents Label1 As Label
 End Class

076d8d6320d917055f2abdb53568482036924175
Nijiho Higuchi <hdc252317@gmail.com>
2026-05-26 20:39:42 +0900

    メニュー画面にProcBのBtnを追加

--- WindowsApp02/FormMenu.Designer.vb
+++ WindowsApp02/FormMenu.Designer.vb
@@ -23,6 +23,8 @@ Partial Class FormMenu
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
         Me.SuspendLayout()
         '
         'Button1
@@ -34,11 +36,22 @@ Partial Class FormMenu
         Me.Button1.Text = "ProcA"
         Me.Button1.UseVisualStyleBackColor = True
         '
+        'Button2
+        '
+        Me.Button2.CausesValidation = False
+        Me.Button2.Location = New System.Drawing.Point(329, 244)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(85, 60)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "ProcB"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
         'FormMenu
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 18.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.Name = "FormMenu"
         Me.Text = "Form1"
@@ -47,4 +60,6 @@ Partial Class FormMenu
     End Sub
 
     Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
 End Class

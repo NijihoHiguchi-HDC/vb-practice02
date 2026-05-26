24289b22a54e4d65711b541612980cf88f8b7329
Nijiho Higuchi <hdc252317@gmail.com>
2026-05-26 20:33:34 +0900

    画面遷移を追加

--- WindowsApp02/FormMenu.vb
+++ WindowsApp02/FormMenu.vb
@@ -1,5 +1,5 @@
 ﻿Public Class FormMenu
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-
+        FormMenuProcA.Show()
     End Sub
 End Class
--- WindowsApp02/FormMenuProcA.vb
+++ WindowsApp02/FormMenuProcA.vb
@@ -1,3 +1,9 @@
 ﻿Public Class FormMenuProcA
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        FormProcAI.Show()
+    End Sub
 
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        FormProcA2.Show()
+    End Sub
 End Class
